28897811e5a8eed45a7f18f9243911b9450d14d8
Lars Talian <l.t.stangebyehansen@gmail.com>
2023-02-10 12:29:01 +0100

    feat: add Passage class
    part 1, task 3 complete

---
+++ src/main/java/edu/ntnu/idatt2001/paths/Passage.java
@@ -0,0 +1,63 @@
+package edu.ntnu.idatt2001.paths;
+
+import java.util.List;
+
+public class Passage {
+    String title;
+    String content;
+    List<Link> links;
+
+    public Passage(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public List<Link> getLinks() {
+        return links;
+    }
+
+    public boolean addLink(Link link) {
+        return links.add(link);
+    }
+
+    public boolean hasLinks(String ref) {
+        return !links.isEmpty();
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Game.Passage{");
+        sb.append("title='").append(title).append('\'');
+        sb.append(", content='").append(content).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Passage)) return false;
+
+        Passage passage = (Passage) o;
+
+        if (!getTitle().equals(passage.getTitle())) return false;
+        if (!getContent().equals(passage.getContent())) return false;
+        return getLinks().equals(passage.getLinks());
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getTitle().hashCode();
+        result = 31 * result + getContent().hashCode();
+        result = 31 * result + getLinks().hashCode();
+        return result;
+    }
+}
