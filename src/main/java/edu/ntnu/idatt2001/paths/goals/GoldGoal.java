509c155f0be9826067d40dbb0e61225821441014
Lars Talian <l.t.stangebyehansen@gmail.com>
2023-02-10 12:58:35 +0100

    feat(goals): add GoldGoal Class in goals package

---
+++ src/main/java/edu/ntnu/idatt2001/paths/goals/GoldGoal.java
@@ -0,0 +1,16 @@
+package edu.ntnu.idatt2001.paths.goals;
+
+import edu.ntnu.idatt2001.paths.Player;
+
+public class GoldGoal implements Goal {
+    private final int minimumGold;
+
+    public GoldGoal(int minimumGold) {
+        this.minimumGold = minimumGold;
+    }
+
+    @Override
+    public boolean isFulfilled(Player player) {
+        return player.getGold() >= minimumGold;
+    }
+}
