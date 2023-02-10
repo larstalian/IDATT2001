0a689ed5173d6a09301776486c1138c2a0324463
Lars Talian <l.t.stangebyehansen@gmail.com>
2023-02-10 12:21:00 +0100

    feat: add Link class
    part 1, task 2 complete

---
+++ src/main/java/edu/ntnu/idatt2001/paths/Link.java
@@ -0,0 +1,54 @@
+package edu.ntnu.idatt2001.paths;
+
+public class Link {
+    String text;
+    String ref;
+    List<Action> actions;
+
+    public Link(String text, String ref) {
+        this.text = text;
+        this.ref = ref;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getRef() {
+        return ref;
+    }
+
+    public List<Action> getActions() {
+        return actions;
+    }
+
+    public boolean addAction(Action action) {
+        return actions.add(action);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Game.Link{");
+        sb.append("text='").append(text).append('\'');
+        sb.append(", ref='").append(ref).append('\'');
+        sb.append(", actions=").append(actions);
+        sb.append('}');
+        return sb.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Link link)) return false;
+
+        if (!getText().equals(link.getText())) return false;
+        return getRef().equals(link.getRef());
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getText().hashCode();
+        result = 31 * result + getRef().hashCode();
+        return result;
+    }
+}
