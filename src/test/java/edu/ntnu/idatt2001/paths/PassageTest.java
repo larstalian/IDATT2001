6513caa06fefac7ccb79f98570ef55d766a79bd9
JacobFI <jacobiqbal2003@gmail.com>
2023-02-14 12:39:31 +0100

    test(passage): add empty body passage test

---
+++ src/test/java/edu/ntnu/idatt2001/paths/PassageTest.java
@@ -0,0 +1,28 @@
+package edu.ntnu.idatt2001.paths;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PassageTest {
+
+  @Test
+  void getTitle() {
+  }
+
+  @Test
+  void getContent() {
+  }
+
+  @Test
+  void getLinks() {
+  }
+
+  @Test
+  void addLink() {
+  }
+
+  @Test
+  void hasLinks() {
+  }
+}
