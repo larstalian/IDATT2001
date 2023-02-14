b2aff57839621dcad5545840ff2b0b8602073f5a
Lars Talian <l.t.stangebyehansen@gmail.com>
2023-02-14 12:30:09 +0100

    test(Link): add empty tests for Link class

---
+++ src/test/java/edu/ntnu/idatt2001/paths/LinkTest.java
@@ -0,0 +1,28 @@
+package edu.ntnu.idatt2001.paths;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LinkTest {
+
+    @Test
+    void getText() {
+    }
+
+    @Test
+    void getRef() {
+    }
+
+    @Test
+    void getActions() {
+    }
+
+    @Test
+    void addAction() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
