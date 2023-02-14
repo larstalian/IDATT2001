ab101963d989924befafaf4b8fab02a870fa714a
JacobFI <jacobiqbal2003@gmail.com>
2023-02-14 12:55:44 +0100

    test(link): add empty body linkTest for getText and getRef

--- src/test/java/edu/ntnu/idatt2001/paths/LinkTest.java
+++ src/test/java/edu/ntnu/idatt2001/paths/LinkTest.java
@@ -8,10 +8,30 @@ class LinkTest {
 
     @Test
     void getText() {
+        // Arrange
+        String expectedText = "hello world";
+        String expectedRef = "HELLO WORLD";
+        Link example = new Link(expectedText, expectedRef);
+
+        // Act
+        String actualText = example.getText();
+
+        // Assert
+        assertEquals(expectedText, actualText);
     }
 
     @Test
     void getRef() {
+        // Arrange
+        String expectedText = "hello world";
+        String expectedRef = "HELLO WORLD";
+        Link example = new Link(expectedText, expectedRef);
+
+        // Act
+        String actualRef = example.getRef();
+
+        // Assert
+        assertEquals(expectedRef, actualRef);
     }
 
     @Test
