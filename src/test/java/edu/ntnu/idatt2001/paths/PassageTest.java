4b25b5dc08e626ebf356fbc0566a9bcd67c102b6
Lars Talian <l.t.stangebyehansen@gmail.com>
2023-02-16 20:09:58 +0100

    test(passage): add test methods to passage class

--- src/test/java/edu/ntnu/idatt2001/paths/PassageTest.java
+++ src/test/java/edu/ntnu/idatt2001/paths/PassageTest.java
@@ -1,28 +1,78 @@
 package edu.ntnu.idatt2001.paths;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import edu.ntnu.idatt2001.paths.story.Link;
+import edu.ntnu.idatt2001.paths.story.Passage;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.is;
+
 class PassageTest {
+    Passage passage;
+    Link link;
 
-  @Test
-  void getTitle() {
-  }
+    @BeforeEach
+    void setUp() {
+        passage = new Passage("A4-110", "Classroom");
+        link = new Link("Go through door", "A4-112");
+    }
 
-  @Test
-  void getContent() {
-  }
+    @Test
+    void testGetTitle() {
+        assertThat(passage.getTitle(), is("A4-110"));
+    }
 
-  @Test
-  void getLinks() {
-  }
+    @Test
+    void testGetContent() {
+        assertThat(passage.getContent(), is("Classroom"));
+    }
 
-  @Test
-  void addLink() {
-  }
+    @Test
+    void testGetLinks() {
+        passage.addLink(link);
+        assertThat(passage.getLinks(), contains(link));
+    }
 
-  @Test
-  void hasLinks() {
-  }
+    @Test
+    void testAddLink_shouldAddLinkToPassage() {
+        assertThat(passage.addLink(link), is(true));
+        assertThat(passage.getLinks(), contains(link));
+    }
+
+    @Test
+    void testHasLinks() {
+        assertThat(passage.hasLinks("Ref"), is(false));
+
+        passage.addLink(link);
+        Link link2 = new Link("Link", "Ref");
+        passage.addLink(link2);
+
+        assertThat(passage.hasLinks("Ref"), is(true));
+    }
+
+    @Test
+    void testToString() {
+        Link link2 = new Link("Go through window", "A4-114");
+        passage.addLink(link);
+        passage.addLink(link2);
+
+        String expected = "Title: A4-110\n" +
+                "Content: Classroom\n" +
+                "Links:\n" +
+                "- Go through door: A4-112\n" +
+                "- Go through window: A4-114\n";
+
+        assertThat(passage.toString(),
+                is(expected));
+    }
+
+    @Test
+    void testToString_shouldReturnPassageWithoutLinksIfThereAreNoLinks() {
+        String expected = "Title: A4-110\n" +
+                "Content: Classroom\n" +
+                "Links:\n";
+        assertThat(passage.toString(), is(expected));
+    }
 }
