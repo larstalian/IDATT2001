216a8ca2648cb7e0fd56aadc2d1182014d457e9b
Lars Talian <l.t.stangebyehansen@gmail.com>
2023-02-20 13:51:39 +0100

    docs(player): add javadoc for player class and methods

--- src/main/java/edu/ntnu/idatt2001/paths/game/Player.java
+++ src/main/java/edu/ntnu/idatt2001/paths/game/Player.java
@@ -3,6 +3,19 @@ package edu.ntnu.idatt2001.paths.game;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * The Player class represents a player in a game. A player has a name, health, score, gold, and an inventory
+ * of items they have collected. The Player class provides methods for accessing and modifying these attributes.
+ *
+ * <p>The player attributes must fall within certain ranges or an IllegalArgumentException will be thrown.
+ * The player's inventory is empty by default and can be populated using the {@link #addToInventory} method.
+ * The players health, score and gold can be modified using their corresponding add methods
+ * and retrieved using get methods.</p>
+ *
+ * <p>The attributes {@link Player#name} and {@link Player#inventory} are immutable and cannot be modified.
+ *
+ * @see Game
+ */
 public class Player {
     private final String name;
     private final List<String> inventory;
@@ -10,6 +23,15 @@ public class Player {
     private int score;
     private int gold;
 
+    /**
+     * Creates a new Player with the given name, health, score, and gold.
+     *
+     * @param name   the name of the player
+     * @param health the starting health of the player, must be between 0 and 1000 (inclusive)
+     * @param score  the starting score of the player, must be between 0 and 1000 (inclusive)
+     * @param gold   the starting amount of gold the player has, must be between 0 and 100000 (inclusive)
+     * @throws IllegalArgumentException if any of the parameter values are invalid
+     */
     public Player(String name, int health, int score, int gold) {
         if (name.length() < 2 || name.length() > 15) {
             throw new IllegalArgumentException("Name cannot be less than 2 or greater than 15 characters");
@@ -30,26 +52,58 @@ public class Player {
         inventory = new ArrayList<>();
     }
 
+    /**
+     * Returns the name of the player.
+     *
+     * @return the name of the player
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Returns the current health of the player.
+     *
+     * @return the current health of the player
+     */
     public int getHealth() {
         return health;
     }
 
+    /**
+     * Returns the current score of the player.
+     *
+     * @return the current score of the player
+     */
     public int getScore() {
         return score;
     }
 
+    /**
+     * Returns the current amount of gold the player has.
+     *
+     * @return the current amount of gold the player has
+     */
     public int getGold() {
         return gold;
     }
 
+    /**
+     * Returns the list of items in the player's inventory.
+     *
+     * @return the list of items in the player's inventory
+     */
     public List<String> getInventory() {
         return inventory;
     }
 
+    /**
+     * Increases the health of the player by the given amount.
+     * The health cannot be less than 0 or greater than 1000.
+     *
+     * @param health the amount to increase the health by
+     * @throws IllegalArgumentException if the resulting health is invalid
+     */
     public void addHealth(int health) {
         if (this.health + health < 0 || this.health + health > 1000) {
             throw new IllegalArgumentException("Health cannot be less than 0 or greater than 1000");
@@ -57,10 +111,20 @@ public class Player {
         this.health += health;
     }
 
+    /**
+     * Increases the current score of the player by the given amount.
+     *
+     * @param score the amount to increase the score by
+     */
     public void addScore(int score) {
         this.score += score;
     }
 
+    /**
+     * Increases the current amount of gold the player has by the given amount.
+     *
+     * @param gold the amount to increase the gold by
+     */
     public void addGold(int gold) {
         this.gold += gold;
     }
@@ -69,6 +133,11 @@ public class Player {
         inventory.add(item);
     }
 
+    /**
+     * Returns a string representation of the player, including their name, health, score, gold, and inventory.
+     *
+     * @return a string representation of the player
+     */
     @Override
     public String toString() {
         return String.format("%-15s %3d HP  %4d PTS  %4d GOLD  INV: %s",
