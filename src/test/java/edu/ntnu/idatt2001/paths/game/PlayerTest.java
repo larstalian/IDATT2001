20d55dfcbabf205761de92ef03a1bf61a1cc7f49
Lars Talian <l.t.stangebyehansen@gmail.com>
2023-02-21 15:55:02 +0100

    test(player): add tests to test error handling and negative values in methods

--- src/test/java/edu/ntnu/idatt2001/paths/game/PlayerTest.java
+++ src/test/java/edu/ntnu/idatt2001/paths/game/PlayerTest.java
@@ -5,7 +5,9 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
+import static edu.ntnu.idatt2001.paths.game.Player.PlayerConstants.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -84,5 +86,60 @@ class PlayerTest {
     @Test
     void testAddHealth_ShouldThrowExceptionWhenHealthIsUnder0() {
         assertThrows(IllegalArgumentException.class, () -> player.addHealth(-101));
+    }
+
+    @Test
+    void testAddHealth_ShouldThrowExceptionWhenHealthIsOverDefined() {
+        assertThrows(IllegalArgumentException.class, () -> player.addHealth(MAX_HEALTH + 1));
+    }
+
+    @Test
+    void addGold_ShouldThrowExceptionWhenGoldIsUnder0() {
+        assertThrows(IllegalArgumentException.class, () -> player.addGold(-1));
+    }
+
+    @Test
+    void addGold_ShouldThrowExceptionWhenGoldIsOverDefined() {
+        assertThrows(IllegalArgumentException.class, () -> player.addGold(MAX_GOLD + 1));
+    }
+
+    @Test
+    void addScore_ShouldThrowExceptionWhenScoreIsUnder0() {
+        assertThrows(IllegalArgumentException.class, () -> player.addScore(-1));
+    }
+
+    @Test
+    void addScore_ShouldThrowExceptionWhenScoreIsOverDefined() {
+        assertThrows(IllegalArgumentException.class, () -> player.addScore(MAX_SCORE + 1));
+    }
+
+    @Test
+    void testAddToInventory_ShouldThrowExceptionWhenInventoryIsFull() {
+        IntStream.range(0, MAX_INVENTORY_SIZE - player.getInventory().size()).forEach(i -> player.addToInventory("Item"));
+        assertThrows(IllegalArgumentException.class, () -> player.addToInventory("Item"));
+    }
+
+    @Test
+    void testAddToInventory_ItemLengthShouldNotBeLongerThanDefined() {
+        String tooLongString = new String(new char[MAX_ITEM_LENGTH + 1]).replace("\0", "a");
+        assertThrows(IllegalArgumentException.class, () -> player.addToInventory(tooLongString));
+    }
+
+    @Test
+    void testAddToInventory_ItemLengthShouldNotBeShorterThanDefined() {
+        String tooShortString = new String(new char[MIN_ITEM_LENGTH - 1]).replace("\0", "a");
+        assertThrows(IllegalArgumentException.class, () -> player.addToInventory(tooShortString));
+    }
+
+    @Test
+    void testNameLengthShouldNotBeLongerThanDefined() {
+        String tooLongString = new String(new char[MAX_NAME_LENGTH + 1]).replace("\0", "a");
+        assertThrows(IllegalArgumentException.class, () -> new Player(tooLongString, 100, 0, 0));
+    }
+
+    @Test
+    void testNameLengthShouldNotBeShorterThanDefined() {
+        String tooShortString = new String(new char[MIN_NAME_LENGTH - 1]).replace("\0", "a");
+        assertThrows(IllegalArgumentException.class, () -> new Player(tooShortString, 100, 0, 0));
     }
 }
