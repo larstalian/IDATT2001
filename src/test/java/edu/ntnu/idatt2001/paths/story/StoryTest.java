0d9933758302d948634a9946bf715359deee2cc5
Lars Talian <l.t.stangebyehansen@gmail.com>
2023-02-21 16:12:05 +0100

    test(story): improve test coverage and readability in methods

--- src/test/java/edu/ntnu/idatt2001/paths/story/StoryTest.java
+++ src/test/java/edu/ntnu/idatt2001/paths/story/StoryTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 
+import static edu.ntnu.idatt2001.paths.story.Story.StoryConstants.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -13,38 +14,37 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class StoryTest {
 
+    Passage passage1;
+    Passage passage2;
     private Story story;
     private Passage openingPassage;
     private List<Passage> passages;
 
     @BeforeEach
     void setup() {
-        // Create a test Story object with an opening passage and two linked passages
+        //Define passages and links for testing
         openingPassage = new Passage("Opening Passage", "This is the opening passage.");
         story = new Story("Test Story", openingPassage);
 
-        Passage passage1 = new Passage("Passage 1", "This is passage 1.");
+        passage1 = new Passage("Passage 1", "This is passage 1.");
         passage1.addLink(new Link("Go to passage 2", "Passage 2"));
-        story.addPassage(passage1);
 
-        Passage passage2 = new Passage("Passage 2", "This is passage 2.");
+        passage2 = new Passage("Passage 2", "This is passage 2.");
         passage2.addLink(new Link("Go to opening passage", "Opening Passage"));
-        story.addPassage(passage2);
 
-        // Create a list of all passages in the story for testing
         passages = new ArrayList<>();
-        passages.add(passage1);
-        passages.add(passage2);
     }
 
     @Test
-    void testConstructor_TitleTooLongOrShortShouldThrowIllegalArgumentException() {
-        Passage passage = new Passage("Opening Passage", "Content");
+    void testConstructor_TitleTooLongShouldThrowIllegalArgumentException() {
+        String tooLongTitle = new String(new char[MAX_TITLE_LENGTH + 1]).replace("\0", "a");
+        assertThrows(IllegalArgumentException.class, () -> new Story(tooLongTitle, passage1));
+    }
 
-        String tooLongTitle = new String(new char[Story.StoryConstants.MAX_TITLE_LENGTH + 1]).replace("\0", "a");
-        assertThrows(IllegalArgumentException.class, () -> new Story(tooLongTitle, passage));
-        String tooShortTitle = new String(new char[Story.StoryConstants.MIN_TITLE_LENGTH - 1]).replace("\0", "a");
-        assertThrows(IllegalArgumentException.class, () -> new Story(tooShortTitle, passage));
+    @Test
+    void testConstructor_TitleTooShortShouldThrowIllegalArgumentException() {
+        String tooShortTitle = new String(new char[MIN_TITLE_LENGTH - 1]).replace("\0", "a");
+        assertThrows(IllegalArgumentException.class, () -> new Story(tooShortTitle, passage1));
     }
 
     @Test
@@ -65,23 +65,22 @@ class StoryTest {
 
     @Test
     void testAddPassage() {
-        Passage newPassage = new Passage("Passage 3", "This is a new passage.");
-        assertThat(story.addPassage(newPassage), is(true));
+        assertThat(story.addPassage(passage1), is(true));
     }
 
     @Test
     void addPassage_ReturnsFalseIfKeyAlreadyExists() {
-        assertThat(story.addPassage(new Passage("Passage 1", "Passage 1")), is(false));
+        story.addPassage(passage1);
+        assertThat(story.addPassage(new Passage(passage1.getTitle(), passage1.getContent())), is(false));
     }
 
 
     @Test
     void testGetPassage() {
-        Link link = new Link("Passage 1", "Passage 1");
-        Passage passage = story.getPassage(link);
-        assertThat("Passage 1", is(passage.getTitle()));
-        assertThat("This is passage 1.", is(passage.getContent()));
-        assertThat(passage.getLinks(), hasItem(new Link("Go to passage 2", "Passage 2")));
+        story.addPassage(passage1);
+        Link link = new Link(passage1.getTitle(), passage1.getTitle());
+        Passage newPassage = story.getPassage(link);
+        assertThat(newPassage, is(passage1));
     }
 
     @Test
@@ -96,6 +95,10 @@ class StoryTest {
 
     @Test
     void testGetPassages() {
+        story.addPassage(passage1);
+        story.addPassage(passage2);
+        passages.add(passage1);
+        passages.add(passage2);
         assertThat(story.getPassages(), containsInAnyOrder(passages.toArray()));
     }
 
@@ -107,6 +110,8 @@ class StoryTest {
 
     @Test
     void testToString() {
+        story.addPassage(passage1);
+        story.addPassage(passage2);
         String expected = "Title: Test Story\n" + "Opening Passage:\n" + "This is the opening passage.\n" + "Passages:\n" + "- Passage 1: This is passage 1.\n" + "- Passage 2: This is passage 2.\n";
         String actual = story.toString();
         assertThat(actual, is(expected));
