3649bdb1f8b9272756d6728a93d9a10c5cefd84d
Lars Talian <l.t.stangebyehansen@gmail.com>
2023-02-22 13:07:08 +0100

    docs(goals): add javadoc to all goal classes
    applies to HealthGoal, GoldGoal, InventoryGoal, ScoreGoal.
    
    Closes #14

--- src/main/java/edu/ntnu/idatt2001/paths/goals/GoldGoal.java
+++ src/main/java/edu/ntnu/idatt2001/paths/goals/GoldGoal.java
@@ -2,13 +2,35 @@ package edu.ntnu.idatt2001.paths.goals;
 
 import edu.ntnu.idatt2001.paths.game.Player;
 
+/**
+ * <p>The <em>GoldGoal</em> class implements the <em>Goal</em> interface and represents a {@link Goal} of the game
+ * based on the minimum amount of gold a {@link Player} must collect.</p>
+ *
+ * <p>The <em>GoldGoal</em> class is a part of the <em>Paths</em> game.
+ * It specifies the minimum amount of gold a player needs to collect to fulfill the goal
+ * by calling the {@link #isFulfilled} method and passing in a Player object.</p>
+ *
+ * @see Goal
+ * @see Player
+ */
 public class GoldGoal implements Goal {
     private final int minimumGold;
 
+    /**
+     * Creates a new <em>GoldGoal</em> with the given minimum gold amount.
+     *
+     * @param minimumGold the minimum amount of gold a player needs to collect to fulfill the goal
+     */
     public GoldGoal(int minimumGold) {
         this.minimumGold = minimumGold;
     }
 
+    /**
+     * Checks if the player has collected at least the minimum amount of gold to fulfill the goal.
+     *
+     * @param player the player to check for the goal fulfillment
+     * @return {@code true} if the player has collected at least the minimum amount of gold, {@code false} otherwise
+     */
     @Override
     public boolean isFulfilled(Player player) {
         return player.getGold() >= minimumGold;
--- src/main/java/edu/ntnu/idatt2001/paths/goals/HealthGoal.java
+++ src/main/java/edu/ntnu/idatt2001/paths/goals/HealthGoal.java
@@ -2,13 +2,36 @@ package edu.ntnu.idatt2001.paths.goals;
 
 import edu.ntnu.idatt2001.paths.game.Player;
 
+/**
+ * <p>The <em>HealthGoal</em> class implements the <em>Goal</em> interface and represents a goal of the game
+ * based on the minimum amount of health a player must have. </p>
+ *
+ * <p>The <em>HealthGoal</em> class is a part of the <em>Paths</em> game.
+ *
+ * <p>It specifies the minimum amount of health a player needs to have to fulfill the goal
+ * by calling the {@link #isFulfilled} method and passing in a Player object.</p>
+ *
+ * @see Goal
+ * @see Player
+ */
 public class HealthGoal implements Goal {
     private final int minimumHealth;
 
+    /**
+     * Creates a new <em>HealthGoal</em> with the given minimum health amount.
+     *
+     * @param minimumHealth the minimum amount of health a player needs to have to fulfill the goal
+     */
     public HealthGoal(int minimumHealth) {
         this.minimumHealth = minimumHealth;
     }
 
+    /**
+     * Checks if the player has at least the minimum amount of health to fulfill the goal.
+     *
+     * @param player the player to check for the goal fulfillment
+     * @return {@code true} if the player has at least the required amount of health {@code false} otherwise
+     */
     @Override
     public boolean isFulfilled(Player player) {
         return player.getHealth() >= minimumHealth;
