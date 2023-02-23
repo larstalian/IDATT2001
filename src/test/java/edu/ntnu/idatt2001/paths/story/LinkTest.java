8482573720ad025f176cc9f68f579ed68136a04d
JacobFI <jacobiqbal2003@gmail.com>
2023-02-23 15:42:19 +0100

    style(linktest): styles reformated LinkTest code for googlecheckstyle

--- src/test/java/edu/ntnu/idatt2001/paths/story/LinkTest.java
+++ src/test/java/edu/ntnu/idatt2001/paths/story/LinkTest.java
@@ -61,14 +61,16 @@ class LinkTest {
   @Test
   void testToString() {
 
-      Link link = new Link("Go to another room", "room 4");
+    Link link = new Link("Go to another room", "room 4");
 
-      Action action1 = player -> System.out.println("Executing 1");
-      Action action2 = player -> System.out.println("Executing 2");
-      link.addAction(action1);
-      link.addAction(action2);
+    Action action1 = player -> System.out.println("Executing 1");
+    Action action2 = player -> System.out.println("Executing 2");
+    link.addAction(action1);
+    link.addAction(action2);
 
-      String expected = "Game.Link{text='Go to another room', ref='room 4', actions=[" + action1 + ", " + action2 + "]}";
-      assertEquals(expected, link.toString());
+    String expected =
+        "Game.Link{text='Go to another room', ref='room 4', actions=[" + action1 + ", " + action2
+            + "]}";
+    assertEquals(expected, link.toString());
   }
 }
