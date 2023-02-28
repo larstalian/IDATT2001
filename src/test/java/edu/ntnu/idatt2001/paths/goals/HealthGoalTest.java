b76ffaf731d58630611891d77fcb962b3c37cf7d
JacobFI <jacobiqbal2003@gmail.com>
2023-02-28 12:11:27 +0100

    style(HealthGoalTest): styles reformated HealthGoalTest for google checkstyle

--- src/test/java/edu/ntnu/idatt2001/paths/goals/HealthGoalTest.java
+++ src/test/java/edu/ntnu/idatt2001/paths/goals/HealthGoalTest.java
@@ -1,32 +1,32 @@
 package edu.ntnu.idatt2001.paths.goals;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
 import edu.ntnu.idatt2001.paths.game.Player;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
-
 class HealthGoalTest {
 
-    private Player player;
-    private HealthGoal healthGoal;
+  private Player player;
+  private HealthGoal healthGoal;
 
-    @BeforeEach
-    void setUp() {
-        player = new Player("PlayerName", 80, 0, 0);
-        healthGoal = new HealthGoal(100);
-    }
+  @BeforeEach
+  void setUp() {
+    player = new Player("PlayerName", 80, 0, 0);
+    healthGoal = new HealthGoal(100);
+  }
 
-    @Test
-    void isFulfilled() {
-        player.addHealth(20);
-        assertThat(healthGoal.isFulfilled(player), is(true));
-    }
+  @Test
+  void isFulfilled() {
+    player.addHealth(20);
+    assertThat(healthGoal.isFulfilled(player), is(true));
+  }
 
-    @Test
-    void isFulfilled_ShouldReturnFalseIfNotFulfilled() {
-        player.addHealth(-20);
-        assertThat(healthGoal.isFulfilled(player), is(false));
-    }
+  @Test
+  void isFulfilled_ShouldReturnFalseIfNotFulfilled() {
+    player.addHealth(-20);
+    assertThat(healthGoal.isFulfilled(player), is(false));
+  }
 }
