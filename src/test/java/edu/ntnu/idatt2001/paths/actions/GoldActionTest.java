7fcc5b14ca29784d1fcdb2fbd74075ba2aff68a3
JacobFI <jacobiqbal2003@gmail.com>
2023-02-28 12:17:42 +0100

    style(GoldActionTest): styles reformated GoldActionTest for google checkstyle

--- src/test/java/edu/ntnu/idatt2001/paths/actions/GoldActionTest.java
+++ src/test/java/edu/ntnu/idatt2001/paths/actions/GoldActionTest.java
@@ -1,25 +1,26 @@
 package edu.ntnu.idatt2001.paths.actions;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
 import edu.ntnu.idatt2001.paths.game.Player;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
-
 class GoldActionTest {
-    private Player player;
-    private GoldAction goldAction;
 
-    @BeforeEach
-    void setUp() {
-        player = new Player("PlayerName", 100, 0, 0);
-        goldAction = new GoldAction(100);
-    }
+  private Player player;
+  private GoldAction goldAction;
 
-    @Test
-    void execute() {
-        goldAction.execute(player);
-        assertThat(player.getGold(), is(100));
-    }
+  @BeforeEach
+  void setUp() {
+    player = new Player("PlayerName", 100, 0, 0);
+    goldAction = new GoldAction(100);
+  }
+
+  @Test
+  void execute() {
+    goldAction.execute(player);
+    assertThat(player.getGold(), is(100));
+  }
 }
