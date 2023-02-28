bbcb1a50ce6d2aac73ff435797df56ca71beccda
JacobFI <jacobiqbal2003@gmail.com>
2023-02-28 12:07:20 +0100

    style(GoldGoalTest): styles reformated GoldGoalTest for google checkstyle

--- src/test/java/edu/ntnu/idatt2001/paths/goals/GoldGoalTest.java
+++ src/test/java/edu/ntnu/idatt2001/paths/goals/GoldGoalTest.java
@@ -1,31 +1,32 @@
 package edu.ntnu.idatt2001.paths.goals;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
 import edu.ntnu.idatt2001.paths.game.Player;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
-
 class GoldGoalTest {
-    private Player player;
-    private Goal goldGoal;
 
-    @BeforeEach
-    void setUp() {
-        player = new Player("PlayerName", 100, 0, 0);
-        goldGoal = new GoldGoal(100);
-    }
+  private Player player;
+  private Goal goldGoal;
 
-    @Test
-    void testIsFulfilled() {
-        player.addGold(100);
-        assertThat(goldGoal.isFulfilled(player), is(true));
-    }
+  @BeforeEach
+  void setUp() {
+    player = new Player("PlayerName", 100, 0, 0);
+    goldGoal = new GoldGoal(100);
+  }
 
-    @Test
-    void testIsFulfilled_ShouldReturnFalseIfNotFulfilled() {
-        player.addGold(50);
-        assertThat(goldGoal.isFulfilled(player), is(false));
-    }
+  @Test
+  void testIsFulfilled() {
+    player.addGold(100);
+    assertThat(goldGoal.isFulfilled(player), is(true));
+  }
+
+  @Test
+  void testIsFulfilled_ShouldReturnFalseIfNotFulfilled() {
+    player.addGold(50);
+    assertThat(goldGoal.isFulfilled(player), is(false));
+  }
 }
