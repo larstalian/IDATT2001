bf1c8bdccee09586db83cd0c18979c09536e1272
JacobFI <jacobiqbal2003@gmail.com>
2023-02-28 12:23:23 +0100

    style(InventoryActionTest): styles reformated InventoryActionTest for google checks

--- src/test/java/edu/ntnu/idatt2001/paths/actions/InventoryActionTest.java
+++ src/test/java/edu/ntnu/idatt2001/paths/actions/InventoryActionTest.java
@@ -1,26 +1,27 @@
 package edu.ntnu.idatt2001.paths.actions;
 
-import edu.ntnu.idatt2001.paths.game.Player;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.everyItem;
 import static org.hamcrest.Matchers.is;
 
+import edu.ntnu.idatt2001.paths.game.Player;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 class InventoryActionTest {
-    private Player player;
-    private InventoryAction inventoryAction;
 
-    @BeforeEach
-    void setUp() {
-        player = new Player("PlayerName", 100, 0, 0);
-        inventoryAction = new InventoryAction("Sword");
-    }
+  private Player player;
+  private InventoryAction inventoryAction;
 
-    @Test
-    void testExecute() {
-        inventoryAction.execute(player);
-        assertThat(player.getInventory(), everyItem(is("Sword")));
-    }
+  @BeforeEach
+  void setUp() {
+    player = new Player("PlayerName", 100, 0, 0);
+    inventoryAction = new InventoryAction("Sword");
+  }
+
+  @Test
+  void testExecute() {
+    inventoryAction.execute(player);
+    assertThat(player.getInventory(), everyItem(is("Sword")));
+  }
 }
