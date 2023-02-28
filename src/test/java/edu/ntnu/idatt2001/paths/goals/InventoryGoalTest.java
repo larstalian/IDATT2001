8022c06524c1b564323a449191c63b7c9403d427
JacobFI <jacobiqbal2003@gmail.com>
2023-02-28 12:20:57 +0100

    style(InventoryGoalTest): styles reformated InventoryGoalTest for google chekcs

--- src/test/java/edu/ntnu/idatt2001/paths/goals/InventoryGoalTest.java
+++ src/test/java/edu/ntnu/idatt2001/paths/goals/InventoryGoalTest.java
@@ -1,36 +1,35 @@
 package edu.ntnu.idatt2001.paths.goals;
 
-import edu.ntnu.idatt2001.paths.game.Player;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
-import java.util.List;
-
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
+import edu.ntnu.idatt2001.paths.game.Player;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 class InventoryGoalTest {
 
-    private Player player;
-    private InventoryGoal inventoryGoal;
+  private Player player;
+  private InventoryGoal inventoryGoal;
 
-    @BeforeEach
-    void setUp() {
-        player = new Player("PlayerName", 100, 0, 0);
-        inventoryGoal = new InventoryGoal(List.of("Sword", "Shield"));
-    }
+  @BeforeEach
+  void setUp() {
+    player = new Player("PlayerName", 100, 0, 0);
+    inventoryGoal = new InventoryGoal(List.of("Sword", "Shield"));
+  }
 
-    @Test
-    void isFulfilled() {
-        player.addToInventory("Sword");
-        player.addToInventory("Shield");
-        assertThat(inventoryGoal.isFulfilled(player), is(true));
+  @Test
+  void isFulfilled() {
+    player.addToInventory("Sword");
+    player.addToInventory("Shield");
+    assertThat(inventoryGoal.isFulfilled(player), is(true));
 
-    }
+  }
 
-    @Test
-    void isFulfilled_ShouldReturnFalseIfNotFulfilled() {
-        player.addToInventory("Sword");
-        assertThat(inventoryGoal.isFulfilled(player), is(false));
-    }
+  @Test
+  void isFulfilled_ShouldReturnFalseIfNotFulfilled() {
+    player.addToInventory("Sword");
+    assertThat(inventoryGoal.isFulfilled(player), is(false));
+  }
 }
