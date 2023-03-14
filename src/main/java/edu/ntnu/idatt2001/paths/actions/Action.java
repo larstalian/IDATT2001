3a788c22ce129b43e30d6143105565ae25aa601d
Lars Talian <l.t.stangebyehansen@gmail.com>
2023-03-14 17:10:11 +0100

    docs(action): add javadoc

--- src/main/java/edu/ntnu/idatt2001/paths/actions/Action.java
+++ src/main/java/edu/ntnu/idatt2001/paths/actions/Action.java
@@ -2,6 +2,18 @@ package edu.ntnu.idatt2001.paths.actions;
 
 import edu.ntnu.idatt2001.paths.game.Player;
 
+/**
+ * The Action interface represents an action that can be performed in the game.
+ * Actions can be executed on a given player object.
+ *
+ * @see Player
+ */
 public interface Action {
+
+    /**
+     * Executes this Action on the given player.
+     *
+     * @param player the player object that the action should be executed on
+     */
     void execute(Player player);
 }
