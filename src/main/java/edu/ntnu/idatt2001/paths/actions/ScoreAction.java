4d4e5cfe0f7243787a023ce05f4facbe0d1bad6c
Lars Talian <l.t.stangebyehansen@gmail.com>
2023-03-14 17:18:06 +0100

    docs(scoreaction): add javadoc

--- src/main/java/edu/ntnu/idatt2001/paths/actions/ScoreAction.java
+++ src/main/java/edu/ntnu/idatt2001/paths/actions/ScoreAction.java
@@ -2,6 +2,12 @@ package edu.ntnu.idatt2001.paths.actions;
 
 import edu.ntnu.idatt2001.paths.game.Player;
 
+/**
+ * The ScoreAction class represents an action that changes the score value of a player.
+ * The amount of score to change is specified in the constructor.
+ *
+ * @see Action
+ */
 public class ScoreAction implements Action {
     private final int scoreChange;
 
@@ -9,6 +15,12 @@ public class ScoreAction implements Action {
         this.scoreChange = points;
     }
 
+    /**
+     * Executes this action on the given player by adding the scoreChange
+     * value to the player's score value.
+     *
+     * @param player the player object to execute the action on
+     */
     @Override
     public void execute(Player player) {
         player.addScore(player.getScore() + scoreChange);
