bdaa8dfb615bfd355a0106002986911ab62fcca3
Lars Talian <l.t.stangebyehansen@gmail.com>
2023-03-15 19:02:46 +0100

    feat(storydeserializer): add storydeserializer to module

--- src/main/java/edu/ntnu/idatt2001/paths/filehandlers/StoryFileHandler.java
+++ src/main/java/edu/ntnu/idatt2001/paths/filehandlers/StoryFileHandler.java
@@ -2,8 +2,6 @@ package edu.ntnu.idatt2001.paths.filehandlers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
-import edu.ntnu.idatt2001.paths.filehandlers.deserializers.LinkDeserializer;
-import edu.ntnu.idatt2001.paths.filehandlers.deserializers.PassageDeserializer;
 import edu.ntnu.idatt2001.paths.filehandlers.deserializers.StoryDeserializer;
 import edu.ntnu.idatt2001.paths.story.Link;
 import edu.ntnu.idatt2001.paths.story.Passage;
