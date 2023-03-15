282cde5d6b6160175740c5d918eece50903ec513
Lars Talian <l.t.stangebyehansen@gmail.com>
2023-03-15 17:11:00 +0100

    feat(storydeserializer): split into multiple methods to increase cohesion and add javadocs

--- src/main/java/edu/ntnu/idatt2001/paths/filehandlers/deserializers/StoryDeserializer.java
+++ src/main/java/edu/ntnu/idatt2001/paths/filehandlers/deserializers/StoryDeserializer.java
@@ -13,8 +13,24 @@ import java.io.IOException;
 import java.util.Iterator;
 import java.util.Map;
 
+/**
+ * A custom deserializer for the Story class, using Jackson library.
+ * This class is responsible for deserializing JSON data into a Story object.
+ *
+ * @see Story
+ * @see edu.ntnu.idatt2001.paths.filehandlers.StoryFileHandler
+ */
 public class StoryDeserializer extends JsonDeserializer<Story> {
 
+    /**
+     * Deserializes JSON data into a Story object.
+     *
+     * @param jsonParser the JSON parser
+     * @param deserializationContext the deserialization context
+     * @return the deserialized Story object
+     * @throws IOException if there is an issue reading from the JSON parser
+     * @throws JsonProcessingException if there is an issue processing the JSON data
+     */
     @Override
     public Story deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException, JsonProcessingException {
         JsonNode node = jsonParser.getCodec().readTree(jsonParser);
@@ -28,10 +44,22 @@ public class StoryDeserializer extends JsonDeserializer<Story> {
 
         while (fields.hasNext()) {
             Map.Entry<String, JsonNode> entry = fields.next();
-            Passage passage = jsonParser.getCodec().treeToValue(entry.getValue(), Passage.class);
+            Passage passage = createPassageFromJsonNode(jsonParser, entry.getValue());
             story.addPassage(passage);
         }
 
         return story;
+    }
+
+    /**
+     * Creates a Passage object from the given JSON node.
+     *
+     * @param jsonParser the JSON parser
+     * @param passageNode the JSON node containing the passage data
+     * @return the created Passage object
+     * @throws JsonProcessingException if there is an issue processing the JSON data
+     */
+    private Passage createPassageFromJsonNode(JsonParser jsonParser, JsonNode passageNode) throws JsonProcessingException {
+        return jsonParser.getCodec().treeToValue(passageNode, Passage.class);
     }
 }
