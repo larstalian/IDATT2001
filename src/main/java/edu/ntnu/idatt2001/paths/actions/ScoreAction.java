f1b50c66923cddf0082050ce937b99b4e73d8e9a
JacobFI <jacobiqbal2003@gmail.com>
2023-03-15 16:42:54 +0100

    style(ScoreAction): reformat to google checkstyle

--- src/main/java/edu/ntnu/idatt2001/paths/actions/ScoreAction.java
+++ src/main/java/edu/ntnu/idatt2001/paths/actions/ScoreAction.java
@@ -3,26 +3,27 @@ package edu.ntnu.idatt2001.paths.actions;
 import edu.ntnu.idatt2001.paths.game.Player;
 
 /**
- * The ScoreAction class represents an action that changes the score value of a player.
- * The amount of score to change is specified in the constructor.
+ * The ScoreAction class represents an action that changes the score value of a player. The amount
+ * of score to change is specified in the constructor.
  *
  * @see Action
  */
 public class ScoreAction implements Action {
-    private final int scoreChange;
 
-    public ScoreAction(int points) {
-        this.scoreChange = points;
-    }
+  private final int scoreChange;
 
-    /**
-     * Executes this action on the given player by adding the scoreChange
-     * value to the player's score value.
-     *
-     * @param player the player object to execute the action on
-     */
-    @Override
-    public void execute(Player player) {
-        player.addScore(player.getScore() + scoreChange);
-    }
+  public ScoreAction(int points) {
+    this.scoreChange = points;
+  }
+
+  /**
+   * Executes this action on the given player by adding the scoreChange value to the player's score
+   * value.
+   *
+   * @param player the player object to execute the action on
+   */
+  @Override
+  public void execute(Player player) {
+    player.addScore(player.getScore() + scoreChange);
+  }
 }
