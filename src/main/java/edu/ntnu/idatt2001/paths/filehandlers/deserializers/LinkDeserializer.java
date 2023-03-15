aca33ab73209fc64dce6bc332bc7683f29d88be6
Lars Talian <l.t.stangebyehansen@gmail.com>
2023-03-15 16:59:30 +0100

    docs(linkdeserializer): add javadoc

--- src/main/java/edu/ntnu/idatt2001/paths/filehandlers/deserializers/LinkDeserializer.java
+++ src/main/java/edu/ntnu/idatt2001/paths/filehandlers/deserializers/LinkDeserializer.java
@@ -8,8 +8,23 @@ import edu.ntnu.idatt2001.paths.story.Link;
 
 import java.io.IOException;
 
+/**
+ * A custom deserializer for the Link class, using Jackson library. This class is responsible for
+ * deserializing JSON data into a Link object.
+ *
+ * @see Link
+ * @see edu.ntnu.idatt2001.paths.filehandlers.StoryFileHandler
+ */
 public class LinkDeserializer extends JsonDeserializer<Link> {
 
+  /**
+   * Deserializes JSON data into a Link object.
+   *
+   * @param jsonParser the JSON parser
+   * @param deserializationContext the deserialization context
+   * @return the deserialized Link object
+   * @throws IOException if there is an issue reading from the JSON parser
+   */
   @Override
   public Link deserialize(JsonParser jsonParser, DeserializationContext deserializationContext)
       throws IOException {
