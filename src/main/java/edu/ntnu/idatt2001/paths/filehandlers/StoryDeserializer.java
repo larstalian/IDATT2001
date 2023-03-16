0452370c0734f6fd849b99fdd49ee8b72f32df98
Lars Talian <l.t.stangebyehansen@gmail.com>
2023-03-16 15:59:04 +0100

    docs(storydeserializer): clarify javadoc

--- src/main/java/edu/ntnu/idatt2001/paths/filehandlers/StoryDeserializer.java
+++ src/main/java/edu/ntnu/idatt2001/paths/filehandlers/StoryDeserializer.java
@@ -13,12 +13,27 @@ import java.util.Iterator;
 import java.util.Map;
 
 /**
- * A custom deserializer for the Story class, using Jackson library. This class is responsible for
- * deserializing JSON data into a Story object. This class is necessary because of the passage map
- * is of type Map<Link, Passage> and Jacksons default deserialization cannot deserialize the map.
+ * A custom deserializer for the {@link Story} class, using the Jackson library. This class is
+ * responsible for deserializing JSON node to a {@link Story} object. The custom
+ * deserialization is necessary due to the passage map in the {@link Story} being of type {@code
+ * Map<Link, Passage>}.
+ *
+ * <p>When serializing use the {@link StorySerializer} class.
+ *
+ * <p>To use the deserializer with an {@link com.fasterxml.jackson.databind.ObjectMapper}.
+ * Register it with a {@link com.fasterxml.jackson.databind.module.SimpleModule} and add the module
+ * to the ObjectMapper. For example:
+ *
+ * <pre>{@code
+ * ObjectMapper objectMapper = new ObjectMapper();
+ * SimpleModule module = new SimpleModule();
+ * module.addDeserializer(Story.class, new StoryDeserializer());
+ * objectMapper.registerModule(module);
+ * }</pre>
  *
  * @see Story
- * @see edu.ntnu.idatt2001.paths.filehandlers.StoryFileHandler
+ * @see edu.ntnu.idatt2001.paths.filehandlers.StoryDeserializer
+ * @see com.fasterxml.jackson.databind.ObjectMapper
  */
 public class StoryDeserializer extends JsonDeserializer<Story> {
 
