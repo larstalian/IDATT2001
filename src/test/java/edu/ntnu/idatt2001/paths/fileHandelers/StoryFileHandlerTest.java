16b80b45e89ba6662b437a8ee637e579732b088c
Lars Talian <l.t.stangebyehansen@gmail.com>
2023-03-16 15:42:02 +0100

    test(StoryFileHandler): add additional test methods to test that links and actions are equal
    equal as in being the same value after being serialized and deserialized

--- src/test/java/edu/ntnu/idatt2001/paths/fileHandelers/StoryFileHandlerTest.java
+++ src/test/java/edu/ntnu/idatt2001/paths/fileHandelers/StoryFileHandlerTest.java
@@ -1,10 +1,9 @@
 package edu.ntnu.idatt2001.paths.fileHandelers;
 
+import edu.ntnu.idatt2001.paths.actions.Action;
 import edu.ntnu.idatt2001.paths.actions.GoldAction;
+import edu.ntnu.idatt2001.paths.actions.HealthAction;
 import edu.ntnu.idatt2001.paths.filehandlers.StoryFileHandler;
-import edu.ntnu.idatt2001.paths.game.Game;
-import edu.ntnu.idatt2001.paths.game.Player;
-import edu.ntnu.idatt2001.paths.goals.ScoreGoal;
 import edu.ntnu.idatt2001.paths.story.Link;
 import edu.ntnu.idatt2001.paths.story.Passage;
 import edu.ntnu.idatt2001.paths.story.Story;
@@ -16,6 +15,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.stream.IntStream;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
@@ -37,7 +37,7 @@ class StoryFileHandlerTest {
   }
 
   @BeforeEach
-  void setUp() {
+  void setUp() throws IOException {
     storyFileHandler = new StoryFileHandler();
 
     Passage openingPassage = new Passage("Opening Passage", "This is the opening passage");
@@ -47,31 +47,73 @@ class StoryFileHandlerTest {
     testStory.addPassage(forestPassage);
     testStory.addPassage(new Passage("Cave", "You are in a cave."));
     testStory.getPassage(new Link("Forest", "Forest")).addLink(new Link("Go to the cave", "Cave"));
-    testStory
-        .getPassage(new Link("Forest", "Forest"))
-        .getLinks()
-        .get(0)
-        .addAction(new GoldAction(10));
+
+    Action goldAction = new GoldAction(10);
+    Action healthAction = new HealthAction(10);
+
+    testStory.getPassage(new Link("Forest", "Forest")).getLinks().get(0).addAction(goldAction);
+    testStory.getPassage(new Link("Forest", "Forest")).getLinks().get(0).addAction(healthAction);
     testStory.addPassage(new Passage("River", "You are by a river."));
+    savedStoryPath = storyFileHandler.getFilePath().resolve(testStory.getTitle() + ".json");
+    storyFileHandler.saveStoryToFile(testStory);
   }
 
   @Test
-  void testSaveAndLoadStory() throws IOException {
-    storyFileHandler.saveStoryToFile(testStory);
-    savedStoryPath = storyFileHandler.getFilePath().resolve(testStory.getTitle() + ".json");
+  void testSaveStory() {
     assertThat("The story file should exist", Files.exists(savedStoryPath), is(true));
-    Game game =
-        new Game(new Player.Builder("player").build(), testStory, List.of(new ScoreGoal(100)));
+  }
 
+  @Test
+  void testLoadStory() throws IOException {
     Story loadedStory = storyFileHandler.loadStoryFromFile(testStory.getTitle());
     assertThat(loadedStory.getTitle(), equalTo(testStory.getTitle()));
+  }
 
+  @Test
+  void testLoadedStoryHasSameOpeningPassageTitle() throws IOException {
+    Story loadedStory = storyFileHandler.loadStoryFromFile(testStory.getTitle());
     assertThat(
         loadedStory.getOpeningPassage().getTitle(),
         equalTo(testStory.getOpeningPassage().getTitle()));
+  }
 
+  @Test
+  void testLoadedStoryHasSameOpeningPassageContent() throws IOException {
+    Story loadedStory = storyFileHandler.loadStoryFromFile(testStory.getTitle());
     assertThat(
         loadedStory.getOpeningPassage().getContent(),
         equalTo(testStory.getOpeningPassage().getContent()));
+  }
+
+  @Test
+  void testLoadedStoryHasTheSamePassages() throws IOException {
+    Story loadedStory = storyFileHandler.loadStoryFromFile(testStory.getTitle());
+    assertThat(loadedStory.getPassages().toArray(), equalTo(testStory.getPassages().toArray()));
+  }
+
+  @Test
+  void testLoadedStoryHasTheSamePassageLinks() throws IOException {
+    Story loadedStory = storyFileHandler.loadStoryFromFile(testStory.getTitle());
+    assertThat(
+        loadedStory.getPassage(new Link("Forest", "Forest")).getLinks().toArray(),
+        equalTo(testStory.getPassage(new Link("Forest", "Forest")).getLinks().toArray()));
+  }
+
+  @Test
+  void testLoadedStoryHasTheSameActions() throws IOException {
+    Story loadedStory = storyFileHandler.loadStoryFromFile(testStory.getTitle());
+
+    List<Action> testActions =
+        testStory.getPassage(new Link("Forest", "Forest")).getLinks().get(0).getActions();
+    List<Action> loadedActions =
+        loadedStory.getPassage(new Link("Forest", "Forest")).getLinks().get(0).getActions();
+
+    assertThat(testActions.size(), equalTo(loadedActions.size()));
+
+    IntStream.range(0, testActions.size())
+        .forEach(
+            i ->
+                assertThat(
+                    testActions.get(i).getClass(), equalTo(loadedActions.get(i).getClass())));
   }
 }
