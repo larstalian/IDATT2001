7c100c4593f472d9a318f65ab310aacf377c56fc
Lars Talian <l.t.stangebyehansen@gmail.com>
2023-03-16 13:06:47 +0100

    feat(goals): add JsonProperty and JsonCreator to variables and constructors
    Not yet implemented game-saving, the change will be necessary when add

--- src/main/java/edu/ntnu/idatt2001/paths/goals/GoldGoal.java
+++ src/main/java/edu/ntnu/idatt2001/paths/goals/GoldGoal.java
@@ -1,29 +1,31 @@
 package edu.ntnu.idatt2001.paths.goals;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import edu.ntnu.idatt2001.paths.game.Player;
 
 /**
- * <p>The <em>GoldGoal</em> class implements the <em>Goal</em> interface and represents a
- * {@link Goal} of the game
- * based on the minimum amount of gold a {@link Player} must collect.</p>
+ * The <em>GoldGoal</em> class implements the <em>Goal</em> interface and represents a {@link Goal}
+ * of the game based on the minimum amount of gold a {@link Player} must collect.
  *
- * <p>The <em>GoldGoal</em> class is a part of the <em>Paths</em> game.
- * It specifies the minimum amount of gold a player needs to collect to fulfill the goal by calling
- * the {@link #isFulfilled} method and passing in a Player object.</p>
+ * <p>The <em>GoldGoal</em> class is a part of the <em>Paths</em> game. It specifies the minimum
+ * amount of gold a player needs to collect to fulfill the goal by calling the {@link #isFulfilled}
+ * method and passing in a Player object.
  *
  * @see Goal
  * @see Player
  */
 public class GoldGoal implements Goal {
 
-  private final int minimumGold;
+  @JsonProperty private final int minimumGold;
 
   /**
    * Creates a new <em>GoldGoal</em> with the given minimum gold amount.
    *
    * @param minimumGold the minimum amount of gold a player needs to collect to fulfill the goal
    */
-  public GoldGoal(int minimumGold) {
+  @JsonCreator
+  public GoldGoal(@JsonProperty int minimumGold) {
     this.minimumGold = minimumGold;
   }
 
@@ -31,8 +33,8 @@ public class GoldGoal implements Goal {
    * Checks if the player has collected at least the minimum amount of gold to fulfill the goal.
    *
    * @param player the player to check for the goal fulfillment
-   * @return {@code true} if the player has collected at least the minimum amount of gold,
-   * {@code false} otherwise
+   * @return {@code true} if the player has collected at least the minimum amount of gold, {@code
+   *     false} otherwise
    */
   @Override
   public boolean isFulfilled(Player player) {
--- src/main/java/edu/ntnu/idatt2001/paths/goals/HealthGoal.java
+++ src/main/java/edu/ntnu/idatt2001/paths/goals/HealthGoal.java
@@ -1,30 +1,32 @@
 package edu.ntnu.idatt2001.paths.goals;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import edu.ntnu.idatt2001.paths.game.Player;
 
 /**
- * <p>The <em>HealthGoal</em> class implements the <em>Goal</em> interface and represents a goal of
- * the game
- * based on the minimum amount of health a player must have. </p>
+ * The <em>HealthGoal</em> class implements the <em>Goal</em> interface and represents a goal of the
+ * game based on the minimum amount of health a player must have.
  *
  * <p>The <em>HealthGoal</em> class is a part of the <em>Paths</em> game.
  *
- * <p>It specifies the minimum amount of health a player needs to have to fulfill the goal
- * by calling the {@link #isFulfilled} method and passing in a Player object.</p>
+ * <p>It specifies the minimum amount of health a player needs to have to fulfill the goal by
+ * calling the {@link #isFulfilled} method and passing in a Player object.
  *
  * @see Goal
  * @see Player
  */
 public class HealthGoal implements Goal {
 
-  private final int minimumHealth;
+  @JsonProperty private final int minimumHealth;
 
   /**
    * Creates a new <em>HealthGoal</em> with the given minimum health amount.
    *
    * @param minimumHealth the minimum amount of health a player needs to have to fulfill the goal
    */
-  public HealthGoal(int minimumHealth) {
+  @JsonCreator
+  public HealthGoal(@JsonProperty int minimumHealth) {
     this.minimumHealth = minimumHealth;
   }
 
@@ -33,7 +35,7 @@ public class HealthGoal implements Goal {
    *
    * @param player the player to check for the goal fulfillment
    * @return {@code true} if the player has at least the required amount of health {@code false}
-   * otherwise
+   *     otherwise
    */
   @Override
   public boolean isFulfilled(Player player) {
