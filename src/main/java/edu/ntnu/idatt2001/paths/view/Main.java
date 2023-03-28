2f7c77f0796c6387db06498b8ecd9283e3d3dbc8
Lars Talian <l.t.stangebyehansen@gmail.com>
2023-03-28 10:36:39 +0200

    feat(view): add Main class

---
+++ src/main/java/edu/ntnu/idatt2001/paths/view/Main.java
@@ -0,0 +1,19 @@
+package edu.ntnu.idatt2001.paths.view;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+  @Override
+  public void start(Stage stage) throws Exception {
+      stage.setTitle("Paths");
+        stage.show();
+  }
+
+
+public static void main(String[] args){
+    launch(args);
+  }
+}
+
