69c6dcc1c097dedc664162d23345095040dd784e
Lars Talian <l.t.stangebyehansen@gmail.com>
2023-03-30 17:13:21 +0200

    feat(view): add text-rolling

--- src/main/java/edu/ntnu/idatt2001/paths/view/Game.java
+++ src/main/java/edu/ntnu/idatt2001/paths/view/Game.java
@@ -1,68 +1,130 @@
 package edu.ntnu.idatt2001.paths.view;
 
+import edu.ntnu.idatt2001.paths.model.story.Link;
 import edu.ntnu.idatt2001.paths.model.story.Passage;
-import java.awt.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.beans.binding.Bindings;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.Region;
-import javafx.scene.layout.StackPane;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.*;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 import javafx.util.Builder;
+import javafx.util.Duration;
 
 public class Game implements Builder<Region> {
 
-  private Passage currentPassage;
-  private edu.ntnu.idatt2001.paths.model.game.Game currentGame;
-  private StringProperty contentBar;
+  private static final StringProperty contentBar = new SimpleStringProperty();
+  private static Passage currentPassage;
+  private static edu.ntnu.idatt2001.paths.model.game.Game currentGame;
+
+  public static void setCurrentGame(edu.ntnu.idatt2001.paths.model.game.Game chosenGame) {
+    currentGame = chosenGame;
+  }
 
   @Override
   public Region build() {
-    BorderPane results = new BorderPane();
-    results.getStyleClass().add("main-menu");
-    results.setCenter(createCenter());
-    results.setBottom(createBottom());
-    return results;
+    currentPassage = currentGame.begin();
+    BorderPane root = createRoot();
+    addSceneListener(root);
+    return root;
+  }
+
+  private BorderPane createRoot() {
+    BorderPane root = new BorderPane();
+    root.getStyleClass().add("main-menu");
+    root.setCenter(createCenter());
+    root.setBottom(createBottom());
+    return root;
   }
 
   private Node createBottom() {
-    StackPane results = new StackPane();
-    Text contentBar = createContentBar();
-    results.getChildren().add(contentBar);
-    return results;
-  }
-
-  private Text createContentBar() {
-    // loop through current passage content
-    return null;
+    VBox bottom = new VBox();
+    ScrollPane contentBarText = createContentBar();
+    contentBarText.getStyleClass().add("content-bar");
+    bottom.getChildren().add(contentBarText);
+    VBox.setVgrow(contentBarText, Priority.ALWAYS);
+    bottom.setAlignment(Pos.BOTTOM_CENTER);
+    return bottom;
   }
 
   private Node createCenter() {
-    StackPane results = new StackPane();
-    results.getChildren().add(createContent());
-    return results;
+    StackPane center = new StackPane();
+    center.getChildren().add(createLinkChoices());
+    return center;
   }
 
-  private Node createContent() {
-    Text results = new Text();
-    results.setText(contentBar.getValue());
-    return results;
+  private ScrollPane createContentBar() {
+    Text contentBarText = new Text();
+    contentBarText.textProperty().bind(contentBar);
+
+    TextFlow textFlow = new TextFlow(contentBarText);
+    textFlow.getStyleClass().add("text-flow");
+
+    ScrollPane scrollPane = new ScrollPane(textFlow);
+    scrollPane.getStyleClass().add("scroll-pane");
+    textFlow
+        .prefWidthProperty()
+        .bind(Bindings.selectDouble(scrollPane.viewportBoundsProperty(), "width"));
+    return scrollPane;
   }
 
   private void createContentString() {
-    contentBar = new SimpleStringProperty();
+    contentBar.set("");
     char[] charArray = currentPassage.getContent().toCharArray();
+    AtomicInteger index = new AtomicInteger(0);
 
-    for (char c : charArray) {
-      contentBar.setValue(contentBar.getValue() + c);
+    Timeline timeline = new Timeline();
+    KeyFrame keyFrame =
+        new KeyFrame(
+            Duration.millis(50),
+            event -> {
+              if (index.get() < charArray.length) {
+                contentBar.set(contentBar.getValue() + charArray[index.get()]);
+                index.incrementAndGet();
+              }
+            });
 
-      try {
-        Thread.sleep(50);
+    timeline.getKeyFrames().add(keyFrame);
+    timeline.setCycleCount(charArray.length);
+    timeline.play();
+  }
 
-      } catch (InterruptedException e) {
-        throw new RuntimeException(e);
-      }
+  private Node createLinkChoices() {
+    VBox choiceBox = new VBox();
+    for (Link link : currentPassage.getLinks()) {
+      Button button = new Button(link.getText());
+      button.setOnAction(
+          e -> {
+            currentPassage = currentGame.go(link);
+            createContentString();
+          });
+      choiceBox.getChildren().add(button);
     }
+    return choiceBox;
+  }
+
+  private void addSceneListener(BorderPane root) {
+    root.sceneProperty()
+        .addListener(
+            new ChangeListener<>() {
+              @Override
+              public void changed(
+                  ObservableValue<? extends Scene> observable, Scene oldValue, Scene newValue) {
+                if (newValue != null) {
+                  createContentString();
+                  root.sceneProperty().removeListener(this);
+                }
+              }
+            });
   }
 }
