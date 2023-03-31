6a4fe91ba684f750648bc5f7ad131ed2ce46daf3
Lars Talian <l.t.stangebyehansen@gmail.com>
2023-03-31 17:55:41 +0200

    feat(game-view): add link choices

--- src/main/java/edu/ntnu/idatt2001/paths/view/Game.java
+++ src/main/java/edu/ntnu/idatt2001/paths/view/Game.java
@@ -2,7 +2,9 @@ package edu.ntnu.idatt2001.paths.view;
 
 import edu.ntnu.idatt2001.paths.model.story.Link;
 import edu.ntnu.idatt2001.paths.model.story.Passage;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
+import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.beans.binding.Bindings;
@@ -10,10 +12,12 @@ import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.*;
 import javafx.scene.text.Text;
@@ -24,8 +28,11 @@ import javafx.util.Duration;
 public class Game implements Builder<Region> {
 
   private static final StringProperty contentBar = new SimpleStringProperty();
+  private static final AtomicBoolean isAnimationSkipped = new AtomicBoolean(false);
   private static Passage currentPassage;
   private static edu.ntnu.idatt2001.paths.model.game.Game currentGame;
+  private Label skipLabel;
+  private VBox links;
 
   public static void setCurrentGame(edu.ntnu.idatt2001.paths.model.game.Game chosenGame) {
     currentGame = chosenGame;
@@ -44,22 +51,36 @@ public class Game implements Builder<Region> {
     root.getStyleClass().add("main-menu");
     root.setCenter(createCenter());
     root.setBottom(createBottom());
+    root.setRight(createRight());
     return root;
   }
 
+  private Node createRight() {
+    StackPane results = new StackPane();
+    results.getChildren().add(createLinkChoices());
+    return results;
+  }
+
   private Node createBottom() {
-    VBox bottom = new VBox();
+    StackPane bottom = new StackPane();
     ScrollPane contentBarText = createContentBar();
     contentBarText.getStyleClass().add("content-bar");
     bottom.getChildren().add(contentBarText);
-    VBox.setVgrow(contentBarText, Priority.ALWAYS);
-    bottom.setAlignment(Pos.BOTTOM_CENTER);
+    StackPane.setAlignment(contentBarText, Pos.BOTTOM_CENTER);
+
+    skipLabel = new Label("(click to skip)");
+    skipLabel.getStyleClass().add("skip-label");
+    skipLabel.setVisible(false);
+
+    bottom.getChildren().add(skipLabel);
+    StackPane.setAlignment(skipLabel, Pos.BOTTOM_RIGHT);
+    StackPane.setMargin(skipLabel, new Insets(0, 10, 10, 0));
+
     return bottom;
   }
 
   private Node createCenter() {
-    StackPane center = new StackPane();
-    center.getChildren().add(createLinkChoices());
+    AnchorPane center = new AnchorPane();
     return center;
   }
 
@@ -68,13 +89,14 @@ public class Game implements Builder<Region> {
     contentBarText.textProperty().bind(contentBar);
     TextFlow textFlow = new TextFlow(contentBarText);
     ScrollPane scrollPane = new ScrollPane(textFlow);
-    
+
     textFlow
         .prefWidthProperty()
         .bind(Bindings.selectDouble(scrollPane.viewportBoundsProperty(), "width"));
-    
+
     scrollPane.getStyleClass().add("scroll-pane");
     textFlow.getStyleClass().add("super-text-flow");
+    scrollPane.setOnMouseClicked(event -> isAnimationSkipped.set(true));
     return scrollPane;
   }
 
@@ -83,34 +105,65 @@ public class Game implements Builder<Region> {
     char[] charArray = currentPassage.getContent().toCharArray();
     AtomicInteger index = new AtomicInteger(0);
 
+    skipLabel.setVisible(true);
+    links.setVisible(false);
+
     Timeline timeline = new Timeline();
     KeyFrame keyFrame =
         new KeyFrame(
-            Duration.millis(50),
+            Duration.millis(30),
             event -> {
               if (index.get() < charArray.length) {
                 contentBar.set(contentBar.getValue() + charArray[index.get()]);
                 index.incrementAndGet();
               }
+              if (isAnimationSkipped.get()) {
+                contentBar.set(currentPassage.getContent());
+                timeline.stop();
+              }
             });
 
     timeline.getKeyFrames().add(keyFrame);
     timeline.setCycleCount(charArray.length);
+
+    timeline
+        .statusProperty()
+        .addListener(
+            (observable, oldValue, newValue) -> {
+              if (newValue == Animation.Status.STOPPED) {
+                skipLabel.setVisible(false);
+                links.setVisible(true);
+                isAnimationSkipped.set(false);
+              }
+            });
+  
     timeline.play();
   }
 
   private Node createLinkChoices() {
-    VBox choiceBox = new VBox();
+    links = new VBox();
+    links.setAlignment(Pos.BOTTOM_CENTER);
+    links.setPrefHeight(VBox.USE_COMPUTED_SIZE);
+    VBox.setMargin(links, new Insets(10, 10, 10, 10));
+    links.getStyleClass().add("link-view");
     for (Link link : currentPassage.getLinks()) {
       Button button = new Button(link.getText());
+      button.setFocusTraversable(true);
+      button.getStyleClass().add("link-button");
       button.setOnAction(
           e -> {
             currentPassage = currentGame.go(link);
             createContentString();
           });
-      choiceBox.getChildren().add(button);
+      links.getChildren().add(button);
     }
-    return choiceBox;
+    AnchorPane anchorPane = new AnchorPane();
+    AnchorPane.setBottomAnchor(links, 0.0);
+    AnchorPane.setLeftAnchor(links, 0.0);
+    AnchorPane.setRightAnchor(links, 0.0);
+    anchorPane.getChildren().add(links);
+
+    return anchorPane;
   }
 
   private void addSceneListener(BorderPane root) {
